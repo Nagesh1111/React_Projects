ae0b677246faafb96c95d6c4803f23265d2dd6cd
=Nagesh <=nageshg429@gmail.com>
2024-04-10 14:32:10 +0530

    varahi farms modified

--- varahifarms/src/App.js
+++ varahifarms/src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import {BrowserRouter ,Routes,Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Components/Home';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import Products from './Components/Products';
 import Faq from './Components/Faq';
 function App() {
-  return (<div>
-  <BrowserRouter>
-  <Header/>
-     <Routes>
-       <Route path='/home' element = {<Home/>}/> 
-       <Route path='/products' element = {<Products/>}/>
-       <Route path='/faq' element = {<Faq/>}/>
-       {/* <Route path='/contact' element = {<Contact/>}/>
-       <Route path='/order' element = {<Track/>}/>   */}
-    </Routes>
-    <Footer/>
-  </BrowserRouter>
-  </div>)
-  
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/faq" element={<Faq />} />
+          {/* <Route path="/contact" element={<Contact />} />
+          <Route path="/order" element={<Track />} /> */}
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
